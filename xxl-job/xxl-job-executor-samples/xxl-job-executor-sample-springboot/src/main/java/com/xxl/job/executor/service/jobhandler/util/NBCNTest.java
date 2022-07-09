11b4b73d5b7c3c8dccd8a62f620267f78c9d5244
dongyangwei <973741309@qq.com>
2022-07-09 17:14:08 +0800

    增加 admin2的代码

--- xxl-job/xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/util/NBCNTest.java
+++ xxl-job/xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/util/NBCNTest.java
@@ -1,10 +1,8 @@
 package com.xxl.job.executor.service.jobhandler.util;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.BiConsumer;
 
 /**
  * @ClassName NBCNTest
@@ -18,22 +16,34 @@ public class NBCNTest {
         Scanner in = new Scanner(System.in);
         int nums = Integer.parseInt(in.nextLine());
         List<String> dict = new ArrayList<>();
+        List<String> querys = new ArrayList<>();
+        Map<String,Integer> result = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < nums; i++) {
             dict.add(in.nextLine());
         }
         int queryNum = Integer.parseInt(in.nextLine());
         for (int i = 0; i < queryNum; i++) {
-            int count = 0;
-            String query = in.nextLine();
-            for (String world:dict) {
-               if (isAnagram(query,world)){
-                count++;
-               }
-            }
-            ans.add(count);
+            String temp = in.nextLine();
+            querys.add(temp);
+            result.put(temp,0);
         }
-        ans.forEach(System.out::println);
+        System.out.println("123");
+        Collections.sort(dict);
+        Collections.sort(querys);
+        int j = 0;
+        for (int i = 0; i < queryNum; i++) {
+            String query = querys.get(i);
+            int count = 0;
+            while (j < nums && isAnagram(query,dict.get(j))){
+                    j++;
+                    count++;
+            }
+            result.put(query,count);
+        }
+        for (Map.Entry entry: result.entrySet()){
+            System.out.println(entry.getValue());
+        }
     }
     public static boolean isAnagram(String str1,String str2){
         if (str1==null || str2==null || str1.length() != str2.length() || str1.equals(str2)){
@@ -43,15 +53,12 @@ public class NBCNTest {
         char[] chars2 = str2.toLowerCase().toCharArray();
         int sum1 = 0;
         int sum2 = 0;
-        for (int i = 0; i < chars1.length; i++) {
-            sum1 += (int) chars1[i];
+        for (char c : chars1) {
+            sum1 += (int) c;
         }
-        for (int i = 0; i < chars2.length; i++) {
-            sum2 += (int) chars1[i];
+        for (char c : chars2) {
+            sum2 += (int) c;
         }
-        if (sum1==sum2){
-            return true;
-        }
-        return false;
+        return sum1 == sum2;
     }
 }
